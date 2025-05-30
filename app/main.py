69c7e6b1a0fc0662646c9b335a7655f0175dca88
Jorge Espinola <yorluis64@hotmail.com>
2025-05-30 00:27:50 -0300

    Quitando archivos estativos, preparando FasApi para manejar la logica de backend, para integrar el frandend de vite y react

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 import uvicorn
 from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv
@@ -20,23 +18,13 @@ app = FastAPI(
     version="0.1.0"
 )
 
-# Montar directorio de archivos estáticos (CSS, JS, imágenes de UI)
-# Asegúrate de que la ruta 'app/views/static' exista o créala.
-# Si 'app/views/static' no existe aún, FastAPI lanzará un error al iniciar.
-# Por ahora, comentaremos esta línea y la descomentaremos cuando creemos los archivos.
-# app.mount("/static", StaticFiles(directory="app/views/static"), name="static")
-
-# Configurar plantillas Jinja2
-# Asegúrate de que la ruta 'app/views/templates' exista o créala.
-templates = Jinja2Templates(directory="app/views/templates")
-
 # Endpoint de prueba
 @app.get("/")
 async def read_root():
     # Redirigir a la página de carga de la aplicación de piel
     return RedirectResponse(url="/skin/")
 
-# Aquí registraremos los routers de los controladores, por ejemplo:
+# Registrar routers de los controladores
 from app.controllers import skin
 app.include_router(skin.router, prefix="/skin", tags=["Skin Analysis Frontend"])
 
