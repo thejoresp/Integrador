d25945225825e6279c6e6cd6af0cf953182ab775
Jorge Espinola <yorluis64@hotmail.com>
2025-06-21 04:21:44 -0300

    Quitando componentes que ya no uso

--- run_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script para levantar el backend en entorno local
-export ENV_FILE=backend/.env.local
-export $(grep -v '^#' $ENV_FILE | xargs)
-uvicorn backend.main:app --host 0.0.0.0 --port 8080 --reload 
