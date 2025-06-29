f458787e41ea46fddcf065f1caacc9643d948ae7
Jorge Espinola <yorluis64@hotmail.com>
2025-06-29 06:19:23 +0000

    Ajustando ENV

--- reboot_backend.sh
+++ reboot_backend.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# Verificar si existe la imagen 'pielsana-backend'
-imagen_id=$(docker images -q pielsana-backend)
+# Eliminar cualquier contenedor llamado 'pielsana-backend'
+sudo docker rm -f pielsana-backend 2>/dev/null
 
+# Eliminar la imagen 'pielsana-backend' si existe
+imagen_id=$(docker images -q pielsana-backend)
 if [ -n "$imagen_id" ]; then
-  read -p "¿Quieres eliminar la imagen 'pielsana-backend'? (s/N): " respuesta
-  if [[ "$respuesta" =~ ^[sS]$ ]]; then
-    echo "Eliminando la imagen 'pielsana-backend'..."
-    sudo docker rmi -f pielsana-backend
-    echo "Construyendo la imagen Docker del backend..."
-    sudo docker build -t pielsana-backend -f backend/Dockerfile .
-  else
-    echo "Usando la imagen existente 'pielsana-backend' para crear el contenedor."
-  fi
-else
-  echo "No existe la imagen 'pielsana-backend'. Construyendo..."
-  sudo docker build -t pielsana-backend -f backend/Dockerfile .
+  echo "Eliminando la imagen 'pielsana-backend'..."
+  sudo docker rmi -f pielsana-backend
 fi
 
-# Ejecutar el contenedor
+echo "Construyendo la imagen Docker del backend..."
+sudo docker build -t pielsana-backend -f backend/Dockerfile .
+
 echo "Ejecutando el backend en Docker..."
 sudo docker run -d --rm --name pielsana-backend -p 8080:8080 pielsana-backend 
