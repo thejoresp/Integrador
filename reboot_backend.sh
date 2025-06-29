d53870d90cdf4a210ff4ae8008122560e8f64110
Jorge Espinola <yorluis64@hotmail.com>
2025-06-29 06:30:48 +0000

    Ajustando ENV

--- reboot_backend.sh
+++ reboot_backend.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Eliminar cualquier contenedor llamado 'pielsana-backend'
+# Forzar la eliminación del contenedor llamado 'pielsana-backend' (si existe)
 sudo docker rm -f pielsana-backend 2>/dev/null
 
-# Eliminar la imagen 'pielsana-backend' si existe
-imagen_id=$(docker images -q pielsana-backend)
-if [ -n "$imagen_id" ]; then
-  echo "Eliminando la imagen 'pielsana-backend'..."
-  sudo docker rmi -f pielsana-backend
-fi
+# Forzar la eliminación de la imagen 'pielsana-backend' (si existe)
+sudo docker rmi -f pielsana-backend 2>/dev/null
 
 echo "Construyendo la imagen Docker del backend..."
 sudo docker build -t pielsana-backend -f backend/Dockerfile .
