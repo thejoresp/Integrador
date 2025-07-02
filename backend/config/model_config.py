2efb5c086281407454bd1a3b307f87147f090e79
Jorge Espinola <yorluis64@hotmail.com>
2025-07-02 02:44:33 +0000

    Modelo acne

--- backend/config/model_config.py
+++ backend/config/model_config.py
@@ -13,6 +13,11 @@ LUNARES_MODEL_PATH = os.getenv(
     os.path.join(BASE_DIR, "backend", "modelos", "ham10000", "lunares.keras")
 )
 
+ACNE_MODEL_PATH = os.getenv(
+    "ACNE_MODEL_PATH",
+    os.path.join(BASE_DIR, "backend", "modelos", "acne", "acne.keras")
+)
+
 # Configuración de TensorFlow
 import tensorflow as tf
 tf.config.set_visible_devices([], 'GPU')  # Forzar uso de CPU
@@ -20,4 +25,5 @@ tf.config.threading.set_inter_op_parallelism_threads(4)  # Ajustar según tu CPU
 tf.config.threading.set_intra_op_parallelism_threads(4)  # Ajustar según tu CPU
 
 # Crear directorios necesarios para modelos locales
-os.makedirs(os.path.dirname(LUNARES_MODEL_PATH), exist_ok=True) 
+os.makedirs(os.path.dirname(LUNARES_MODEL_PATH), exist_ok=True)
+os.makedirs(os.path.dirname(ACNE_MODEL_PATH), exist_ok=True) 
